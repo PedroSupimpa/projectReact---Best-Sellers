3a5aac2cde4e9a300dbeec205119c3dadb8ea67b
PedroSouza <shiro2303@gmail.com>
2023-04-19 17:20:34 -0300

    project done

---
+++ src/books.js
@@ -0,0 +1,33 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+import img4 from "./images/book-4.jpg";
+
+export const books = [
+  {
+    title: "The Last Thing He Told Me: A Novel",
+    author: "Laura Dave",
+    img: img1,
+    id: 1,
+  },
+  {
+    title:
+      "You Can't Joke About That: Why Everything Is Funny, Nothing Is Sacred, and We're All in This Together",
+    author: "Kat Timpf",
+    img: img2,
+    id: 2,
+  },
+  {
+    title: "The Wager: A Tale of Shipwreck, Mutiny and Murder Hardcover",
+    author: "David Grann",
+    img: img3,
+    id: 3,
+  },
+  {
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    author: "James Clear",
+    img: img4,
+    id: 4,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,44 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-
-const firstBook = {
-  title: "The Last Thing He Told Me: A Novel",
-  author: "Laura Dave",
-  img: "./images/book-1.jpg",
-};
-
-const secondBook = {
-  title:
-    "You Can't Joke About That: Why Everything Is Funny, Nothing Is Sacred, and We're All in This Together",
-  author: "Kat Timpf",
-  img: "./images/book-2.jpg",
-};
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
   return (
-    <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
-    </section>
-  );
-};
-
-const Book = ({ title, author, img }) => {
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
+    <>
+      <h1>Amazon Best Sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
